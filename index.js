4746ce588cae4dcf7d4ff9efc10929d9e3727572
Alejandra Landa <aleriquene@gmail.com>
2022-02-25 12:17:55 -0600

    commit tree 2a92baa4186fb63d8b737d647d701c208566106a

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
-// Code your solutions in this file
+
+function writeCards(names, event) {
+
+  const messages = [];
+   for (let i = 0; i < names.length; i++) {
+     const message = `Thank you, ${names[i]}, for the wonderful ${event} gift!`;
+     messages.push(message);
+   }
+   return messages;
+ 
+ }
+ console.log(writeCards(['Bob', 'Sue'], 'wedding'));
+
+function countDown(){
+    for(let num=0; num <11;num++){
+      console.log(num)
+    }
+}
